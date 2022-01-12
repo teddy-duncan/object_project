41eb9233289d3110a751a7bc00cf70695877e6c0
teddy-duncan <77077101+teddy-duncan@users.noreply.github.com>
2022-01-12 18:47:57 -0500

    Add files via upload

---
+++ main copy 2.js
@@ -0,0 +1,30 @@
+img="";
+status="";
+function preload(){
+    img=loadImage("mobile.jpeg");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+function draw(){
+    image(img,0,0,640,420);
+    fill("red");
+    text("mobile",45,75);
+    noFill();
+    stroke("red");
+    rect(30,60,450,350);
+}
